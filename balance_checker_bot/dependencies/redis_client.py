4e97b1def64fd8198475da89120fc6828cbace6f
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-01-21 17:38:11 +0000

    fix broken redis singleton

--- balance_checker_bot/dependencies/redis_client.py
+++ balance_checker_bot/dependencies/redis_client.py
@@ -1,5 +1,22 @@
+import redis
+
+from balance_checker_bot.config import get_settings
+
+
 class RedisClient:
     _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not RedisClient._instance:
+            cls._instance = super(RedisClient, cls).__new__(cls, *args, **kwargs)
+            settings = get_settings()
+            cls._instance.connection = redis.Redis(
+                host=settings.REDIS_HOST,
+                port=6379,  # Or make this configurable as well
+                decode_responses=True,
+                password=settings.REDIS_PASSWORD,
+            )
+        return cls._instance.connection
 
     @staticmethod
     def set_mock_connection(mock_redis):
